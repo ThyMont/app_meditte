a4aefc1e37ff0bf3bd709be53688c1da67e50d79
Thyago Monteiro <tmacademico@gmail.com>
2021-04-13 22:58:00 -0300

    Criação da classe Music, criação de lista de músicas e inserção dos arquivos das músicas (mp3 e imagem)

--- app/src/main/java/com/thymont/meditte/MainActivity.java
+++ app/src/main/java/com/thymont/meditte/MainActivity.java
@@ -3,13 +3,52 @@ package com.thymont.meditte;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<Music> musicList;
+    private Button startButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Cliquei no botão", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        musicList = new ArrayList<>();
+        fillMusicList();
+
+
+    }
+
+    public void openMusicList(View view){
+
+    }
+
+    private void fillMusicList(){
+        Music m1 = new Music(R.raw.dox_lonely_red_tree, "Dox", "Lonely Red Tree", "Lonely Red Tree by Dox | https://soundcloud.com/dox-free-music\n" +
+                "Music promoted by https://www.free-stock-music.com\n" +
+                "Attribution-NoDerivs 3.0 Unported (CC BY-ND 3.0)\n" +
+                "https://creativecommons.org/licenses/by-nd/3.0/", R.drawable.dox_lonely_red_tree);
+        Music m2 = new Music(R.raw.keys_of_moon_tropical_aura, "Keys of Moon", "Tropical Aura", "Tropical Aura by Keys of Moon | https://soundcloud.com/keysofmoon\n" +
+                "Music promoted by https://www.free-stock-music.com\n" +
+                "Attribution 4.0 International (CC BY 4.0)\n" +
+                "https://creativecommons.org/licenses/by/4.0/", R.drawable.keys_of_moon_tropical_aura );
+        Music m3 = new Music(R.raw.scott_buckley_borealis, "Scott Buckley", "Borealis", "Borealis by Scott Buckley | https://soundcloud.com/scottbuckley\n" +
+                "Music promoted by https://www.free-stock-music.com\n" +
+                "Attribution 4.0 International (CC BY 4.0)\n" +
+                "https://creativecommons.org/licenses/by/4.0/", R.drawable.scott_buckley_borealis );
+        musicList.addAll(Arrays.asList(m1, m2, m3));
     }
 }
---
+++ app/src/main/java/com/thymont/meditte/Music.java
@@ -0,0 +1,62 @@
+package com.thymont.meditte;
+
+import java.io.Serializable;
+
+public class Music implements Serializable {
+    private Integer id;
+    private String artist;
+    private String name;
+    private String description;
+    private Integer image;
+
+    public Music() {
+    }
+
+    public Music(Integer id, String artist, String name, String description, Integer image) {
+        this.id = id;
+        this.artist = artist;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getImage() {
+        return image;
+    }
+
+    public void setImage(Integer image) {
+        this.image = image;
+    }
+}
